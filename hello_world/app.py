1905fcc12324e14d835dbeb5e0186248e89c4e91
Godwin Rose Samuel <godwinrs@amazon.com>
2024-03-05 13:41:38 -0800

    updated text

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello testing aws",
+            "message": "hello testing aws from git",
             # "location": ip.text.replace("\n", "")
         }),
     }
